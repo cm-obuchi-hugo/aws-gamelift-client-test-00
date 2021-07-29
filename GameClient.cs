af3f12b58441f4b0c402af71631c4e9d24926402
Hugo Obuchi <76051977+hugo-obuchi@users.noreply.github.com>
2021-07-30 01:24:47 +0900

    Neatened the code and added some comments

--- GameClient.cs
+++ GameClient.cs
@@ -4,23 +4,36 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 
+using Amazon;
 using Amazon.GameLift;
 using Amazon.GameLift.Model;
+using Amazon.CognitoIdentity;
 
 
 namespace AWSGameLiftClientTest
 {
     class GameClient
     {
+        // A UTF-8 encoder to process byte[] <-> string conversion
         public readonly System.Text.Encoding Encoder = System.Text.Encoding.UTF8;
+
+        // .NET Socket TCP client
         private TcpClient client = null;
+
+        // NetworkStream(derives from Stream class) of a TCP client
         private NetworkStream stream = null;
+        
+        // A unique player id will be generated later
         private string playerId = string.Empty;
+
+        // All the GameLift related things will be processed by this AmazonGameLiftClient
         private AmazonGameLiftClient gameLiftClient = null;
 
+        // Sessions contain general information
         private GameSession gameSession = null;
         private PlayerSession playerSession = null;
 
+        // A instance's status flag of this class 
         public bool IsAlive { get; private set; } = false;
 
 
@@ -29,21 +42,31 @@ namespace AWSGameLiftClientTest
             IsAlive = true;
 
             playerId = Guid.NewGuid().ToString();
-            gameLiftClient = new AmazonGameLiftClient("fake", "fake", new AmazonGameLiftConfig() { ServiceURL = "http://localhost:8080" });
 
+            Console.WriteLine($"Client : playerId {playerId}");
 
+            // gameLiftClient = new AmazonGameLiftClient("fake", "fake", new AmazonGameLiftConfig() { ServiceURL = "http://localhost:8080" });
+            
+            CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+                "Your-Identity-Pool-ID", // Identity pool ID
+                RegionEndpoint.APNortheast1 // Region
+            );
+
+            gameLiftClient = new AmazonGameLiftClient(credentials, RegionEndpoint.APNortheast1);
         }
 
         public void Start()
         {
+            // Create GameSession(async) -> Create PlayerSession(async) -> Connect ()
             CreateSessionsAndConnect();
-
         }
 
         async private Task CreateSessionsAndConnect()
         {
             await CreateGameSessionAsync();
             await CreatePlayerSessionAsync();
+
+            // Connect to the IP provided by PlayerSession
             Connect();
         }
 
@@ -53,7 +76,7 @@ namespace AWSGameLiftClientTest
 
             // GameSession gameSession = await CreateGameSessionAsync();
             var request = new CreateGameSessionRequest();
-            request.FleetId = "fake";
+            request.FleetId = "fleet-id";
             request.CreatorId = playerId;
             request.MaximumPlayerSessionCount = 1;
 
@@ -103,6 +126,12 @@ namespace AWSGameLiftClientTest
             }
         }
 
+
+        // Connect to the IP which PlayerSession provides
+        // When client connects : 
+        // 1) Receive the msg sent by server
+        // 2) Send another msg back
+        // 3) Close the connection
         private void Connect()
         {
             Console.WriteLine($"Client : Connect() start");
@@ -117,16 +146,19 @@ namespace AWSGameLiftClientTest
 
                 stream = client.GetStream();
 
+                // A binary buffer for message
                 byte[] msg = new byte[256];
 
+                // Read the message (Stream.Read() blocks)
                 Console.WriteLine($"Client : Wait to read from stream");
                 while (stream.Read(msg) > 0)
                 {
+                    // Decode the binary msg to string
                     string str = Encoder.GetString(msg);
                     Console.WriteLine($"Received : {str}");
 
+                    // Encode the string to binary message and send it
                     msg = Encoder.GetBytes("Hello Server, this is Client.");
-
                     stream.Write(msg);
 
                     Console.WriteLine($"Client : Message sent");
@@ -135,8 +167,9 @@ namespace AWSGameLiftClientTest
                 }
 
             }
-
-            if (client != null) client.Close();
+            
+            // After a successfule connection, close it
+            client.Close();
 
             IsAlive = false;
         }
